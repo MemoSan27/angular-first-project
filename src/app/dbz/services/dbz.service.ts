3e7a6d95a6de7ec542d0d47b04c1d616923afd16
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-02-23 12:33:15 -0700

    app finished

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -20,7 +20,7 @@ export class DbzService {
     power: 5000,
   }];
 
-  onNewCharacter( character: Character):void {
+  addCharacter( character: Character):void {
 
     const newCharacter: Character = { id: uuid(), ...character };
     this.characters.push(newCharacter);
